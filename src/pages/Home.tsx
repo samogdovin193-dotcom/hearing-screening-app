e7093d65dbe21a7d6216f1b034219ab4ddbe49fd
Samuel Gdovin <samogdovin193@gmail.com>
2026-06-25 12:01:36 +0200

    Fix robot gif

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -67,6 +67,7 @@ export default function Home() {
           <img
             src="/assets/sk/images/default.gif" 
             alt="Robot Tomáš" 
+            className='robot-img'
             onClick={() => go("/select")}
           />
         </div>
